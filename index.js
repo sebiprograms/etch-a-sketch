24bc071d2371c976f164261adc329b213f39b156
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-03-08 00:08:39 -0600

    Add eventlistener
    boxes now have an eventlistener that turns them
    black if empty and white if not.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const game = document.querySelector("#game")
-game.style.width = "256px"
-game.style.height = "256px"
+game.style.width = "252px"
+game.style.height = "252px"
 game.style.display = "flex"
 game.style.flexWrap = "wrap"
 
-for (let i = 0; i < 256; i++){
+for (let i = 0; i < 252; i++){
   const square = document.createElement("div")
   square.style.border = "solid"
   square.style.borderWidth = ".2px"
   square.style.borderColor = "grey"
   square.style.width = "16px"
   square.style.height = "16px"
+  square.addEventListener("click", () => {
+    if (square.style.backgroundColor == ""){
+      square.style.backgroundColor = "black"
+    } else {
+      square.style.backgroundColor = "white"
+    }
+  })
   game.appendChild(square)
 }
