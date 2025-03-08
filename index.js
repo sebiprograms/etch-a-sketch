f7e756988e6f4ff696c950f1a890b9cb28a0fb07
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-03-08 00:17:46 -0600

    Rgba values
    Added rgba value to reduce opacity of box border

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const game = document.querySelector("#game")
-game.style.width = "252px"
-game.style.height = "252px"
+game.style.width = "256px"
+game.style.height = "256px"
 game.style.display = "flex"
 game.style.flexWrap = "wrap"
 
-for (let i = 0; i < 252; i++){
+for (let i = 0; i < 256; i++){
   const square = document.createElement("div")
   square.style.border = "solid"
-  square.style.borderWidth = ".2px"
-  square.style.borderColor = "grey"
+  square.style.borderWidth = ".1px"
+  square.style.borderColor = "rgba(3, 3, 3, 0.3)"
   square.style.width = "16px"
   square.style.height = "16px"
   square.addEventListener("click", () => {
