93276eb3346bdf9be2beaff9ae2a7265accd589f
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-03-08 22:18:24 -0600

    Remove Better
    Func removes all children of specified element.
    Helps me resize the drawing board.

--- index.js
+++ index.js
@@ -6,36 +6,71 @@ game.style.display = "flex"
 game.style.flexWrap = "wrap"
 let boxes = 256
 
-
-
-
-for (let i = 0; i < boxes; i++){
-  const square = document.createElement("div")
-  square.className = "square"
-  square.style.border = "solid"
-  square.style.borderWidth = ".1px"
-  square.style.borderColor = "rgba(3, 3, 3, 0.3)"
-  square.style.width = "16px"
-  square.style.height = "16px"
-  square.addEventListener("click", () => {
-    if (square.style.backgroundColor == ""){
-      square.style.backgroundColor = "black"
-    } else {
-      square.style.backgroundColor = ""
-    }
-  })
-  game.appendChild(square)
+// Sets aspect ratio to 16x16
+function sixteen(){
+  removeBetter()
+  for (let i = 0; i < boxes; i++){
+    const square = document.createElement("div")
+    square.className = "square"
+    square.style.border = "solid"
+    square.style.borderWidth = ".1px"
+    square.style.borderColor = "rgba(3, 3, 3, 0.3)"
+    square.style.width = "16px"
+    square.style.height = "16px"
+    square.addEventListener("click", () => {
+      if (square.style.backgroundColor == ""){
+        square.style.backgroundColor = "black"
+      } else {
+        square.style.backgroundColor = ""
+      }
+    })
+    game.appendChild(square)
+  }
 }
 
+// its like the remove function but swag
+function removeBetter(){
+  while(game.lastElementChild){
+    game.removeChild(game.lastElementChild)
+  }
+}
 
+function thirtytwo(){
+  removeBetter()
+  boxes = 5
+  for (let i = 0; i < boxes; i++){
+    const square = document.createElement("div")
+    square.className = "square"
+    square.style.border = "solid"
+    square.style.borderWidth = ".1px"
+    square.style.borderColor = "rgba(3, 3, 3, 0.3)"
+    square.style.width = "16px"
+    square.style.height = "16px"
+    square.addEventListener("click", () => {
+      if (square.style.backgroundColor == ""){
+        square.style.backgroundColor = "black"
+      } else {
+        square.style.backgroundColor = ""
+      }
+    })
+    game.appendChild(square)
+  }
+}
+
+sixteen()
+thirtytwo()
 
 const label = document.createElement("label")
 const select = document.createElement("select")
-const option = document.createElement("option")
-option.textContent = "16 x 16"
+const option1 = document.createElement("option")
+const option2 = document.createElement("option")
+option1.textContent = "16 x 16"
+option2.textContent = "32 x 32"
 label.textContent = "Aspect Ratio"
 
-select.appendChild(option)
+select.appendChild(option1)
+select.appendChild(option2)
 announcement.appendChild(label)
-
 label.appendChild(select)
+
+option1.addEventListener("click", )
